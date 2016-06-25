6524c2d00967ce4149d00b3872aebd956c16b954
Adam Yi <adamyi@live.com>
2016-06-25 19:07:02 +0800

    Initial Version

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# v6dns
+v6dns
+------
 A DNS Server which let IPv4 first devices to use IPv6 first.
+
+### Installation
+
+First make sure you have a safe and uncensored connection to 8.8.8.8, and then run the following command:
+
+```
+npm -g install v6dns
+```
+
+### Usage
+
+```
+sinodns [port] [address]
+```
+The command above will run a DNS relay listening on address:port, treating type A queries specially as mentioned below.
+
+By default, the address is 127.0.0.1:53
+
+It works better with cache. (e.g. using dnsmasq to forward)
+
+### Procedure
+
+Here's how it works:
+
+ * If it's a type A query, send an AAAA query to Google's server
+ * If there's an AAAA record, forward it to client; if not, send the A query
+ * For other types of queries, redirect to Google's server as well
+ * Send the result to client
---
+++ v6dns.js
@@ -0,0 +1,126 @@
+#! /usr/bin/env node
+
+"use strict";
+
+var dns = require('native-dns'),
+  async = require('async'),
+  tcpserver = dns.createTCPServer(),
+  server = dns.createServer();
+
+var onMessage = function (request, response) {
+  console.log('request from:', request.address);
+
+  var f = [];
+  request.question.forEach( function(question) {
+    if ( question.type == dns.consts.NAME_TO_QTYPE.A ) {
+      f.push(function(cb){ testAAAA(question, f, response, cb)});
+      //question.type = dns.consts.NAME_TO_QTYPE.A;
+      //console.log('res:', res);
+      //if (!res) {
+      //  console.log('Searching A');
+      //  f.push(function(cb){ proxy(question, response, cb)});
+      //}
+      
+    }
+    else {
+      f.push(function(cb){ proxy(question, response, cb)});
+    }
+  });
+
+  async.series(f, function() { response.send(); });
+};
+
+var onError = function (err, buff, req, res) {
+  console.log(err.stack);
+};
+
+var onListening = function () {
+  console.log('server listening on', this.address());
+  //this.close();
+};
+
+var onSocketError = function (err, socket) {
+  console.log(err);
+};
+
+var onClose = function () {
+  console.log('server closed', this.address());
+};
+
+var onTimeout = function() {
+  console.log('Time out');
+};
+
+var authority = { address: '114.114.114.114' , port: 53, type: 'udp' }
+
+var proxy = function(question, response, cb) {
+  console.log('proxying', question.name);
+  var request = dns.Request({
+    question: question,
+    server: authority,
+    timeout: 1000
+  });
+
+  request.on('message', function(err, msg) {
+    //console.log(msg);
+    msg.answer.forEach(function(a){ response.answer.push(a)});
+  });
+  
+  request.on('error', onError);
+  request.on('timeout', onTimeout);
+
+  request.on('end', cb);
+  request.send();
+}
+
+var testAAAA = function(question, f, response, cb) {
+  console.log('Testing AAAA for', question.name);
+  var request = dns.Request({
+    question: question,
+    server: authority,
+    timeout: 1000
+  });
+  request.question.type = dns.consts.NAME_TO_QTYPE.AAAA;
+
+  request.on('message', function(err, msg) {
+    //console.log(msg.answer.length);
+    question.type = dns.consts.NAME_TO_QTYPE.A;
+    var t = true;
+    msg.answer.forEach(function(a) {
+      if ( a.type == dns.consts.NAME_TO_QTYPE.AAAA ) //To Solve the IPv4 Only CNAME Case
+      {
+        t = false;
+      }
+    });
+    if (t)
+    {
+      proxy(question, response, cb);
+    }
+    else {
+      cb();
+    }
+  });
+
+  request.on('error', onError);
+  request.on('timeout', onTimeout);
+
+  request.send();
+
+  //question.type = dns.consts.NAME_TO_QTYPE.A;
+}
+
+server.on('request', onMessage);
+server.on('error', onError);
+server.on('listening', onListening);
+server.on('socketError', onSocketError);
+server.on('close', onClose);
+
+server.serve(process.argv[2] || 53, process.argv[3] || '127.0.0.1');
+
+tcpserver.on('request', onMessage);
+tcpserver.on('error', onError);
+tcpserver.on('listening', onListening);
+tcpserver.on('socketError', onSocketError);
+tcpserver.on('close', onClose);
+
+tcpserver.serve(process.argv[2] || 53, process.argv[3] || '127.0.0.1');
